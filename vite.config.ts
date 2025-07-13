ecc107135182cc98198b8da37152cb937531502b
matthewcla <133473967+matthewcla@users.noreply.github.com>
2025-07-12 21:30:06 -0400

    Configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'node',
+    includeSource: ['src/**/*.{ts,tsx}']
+  }
 });
